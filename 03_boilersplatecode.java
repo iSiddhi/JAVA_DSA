f59367062623196be3b3ac0b9049daf81a63bfb1
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-03 01:14:53 +0530

    Create 03_boilersplatecode.java

---
+++ 03_boilersplatecode.java
@@ -0,0 +1,15 @@
+public class main{
+  public static void main(String[] args) {
+
+    
+    
+  }
+}
+
+
+//boilerplate code- code which is compulsary to write.
+// public class main{
+//   public static void main(String[] args) {
+    
+//   }
+// }
