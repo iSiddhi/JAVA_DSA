a7ff4bd9a079753ceada76e5db8046a41ad25d8e
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-03 01:34:06 +0530

    Create 04_output.java

---
+++ 04_output.java
@@ -0,0 +1,12 @@
+public class output {
+    public static void main(String[] args) {
+        System.out.println("Hello world");//line ka soace 
+        System.out.println("Hello world");
+    }
+    
+}
+// print-- function
+//println--line ka space
+//\n= prinln
+
+//
