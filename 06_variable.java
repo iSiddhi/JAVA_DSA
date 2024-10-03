90b5703148f9c7f7c6e87474f0cf69b71ad0254d
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-03 23:02:50 +0530

    Create 06_variable.java

---
+++ 06_variable.java
@@ -0,0 +1,22 @@
+public class variable {
+    public static void main(String[] args) {
+        int a=10;
+        int b= 5;
+        String name = " siddhi ";
+        System.out.println(2*(a+b)); 
+        short n= 2;
+
+          
+    }
+    
+}
+//memory 
+// size of data
+//1 byte= 8 bites [-128 to 127]
+// short,char,= 2 bytes
+// boolean= 1 byte
+// int = 4 bytes
+// long= 8 bytes
+// float = 4 bytes
+// double = 8 bytes
+
