e8ec8a6de23ba4fa9ed88145c97294ba8de287a5
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-03 23:03:29 +0530

    Create 07_DataType.java

---
+++ 07_DataType.java
@@ -0,0 +1,34 @@
+public class dataType {
+public static void main(String[] args) {
+    byte b=8;
+    System.out.println(b);
+    char ch = 'a';
+    System.out.println(ch);
+    boolean var= false;
+    float price= 10.5f;
+    int number= 10;
+    //long = 
+    //double = 
+    short n = 240;
+
+
+
+
+
+}
+    
+}
+
+
+//primitive - byte,short,char,int,float,double
+// non primitive- string,array,class,object.interface
+//memory 
+// size of data
+//1 byte= 8 bites [-128 to 127]
+// short,char,= 2 bytes
+// boolean= 1 byte
+// int = 4 bytes
+// long= 8 bytes
+// float = 4 bytes
+// double = 8 bytes
+
