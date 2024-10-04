18146a62cd5ac123a95586b7c8cc033489fe0b67
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-04 22:58:13 +0530

    Create 13_TypeCasting.java

---
+++ 13_TypeCasting.java
@@ -0,0 +1,9 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        float a = 25.0f;
+        int b = int(a);
+    }
+    
+}
+// explicit conversion
+// chotha kr deta hai
