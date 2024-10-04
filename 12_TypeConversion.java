703c0155c6c31da08c61752469119a43b8ce5ca3
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-04 22:57:25 +0530

    Create 12_TypeConversion.java

---
+++ 12_TypeConversion.java
@@ -0,0 +1,24 @@
+public class TyepConversion {
+    public static void main(String[] args) {
+        // int a = 25;
+        // long b = a;
+        // System.out.println(b);
+
+        
+
+    }
+    
+}
+
+
+
+
+
+
+
+
+/*comparision happens when
+a.type compatible
+b.destination type>source type
+
+byte>short>int>float>long>double*/ 
