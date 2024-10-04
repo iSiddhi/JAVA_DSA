ff2c4487f1d753e21b141ab190ce67dd30ee13fa
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-04 22:56:53 +0530

    Create 11_Product.java

---
+++ 11_Product.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class product {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a*b;
+        System.out.println(product);
+    }
+    
+}
+
+// product of 2 numbers 
+// siddhiPatel096@gmail.com
