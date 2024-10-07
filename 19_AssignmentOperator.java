53d972d119e99620315134b01d26104f0da19361
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-07 23:06:56 +0530

    Create 19_AssignmentOperator.java

---
+++ 19_AssignmentOperator.java
@@ -0,0 +1,22 @@
+public class AssignmentOperator {
+    public class ArithmaticOperators {
+    
+        public static void main(String[] args) {
+    
+            int a=10;
+          int   b=20;
+          System.out.println(a=b);
+          System.out.println(a+=b);
+          System.out.println(a-=b);
+          System.out.println(a*=b);
+          System.out.println(a/b);
+          
+        }
+    }
+    
+    
+    
+    
+}
+
+// Assignment operator
