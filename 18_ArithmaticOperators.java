7b8e0e15e6d34a367fa0ad147018517e12116523
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-07 23:06:18 +0530

    Create 18_ArithmaticOperators.java

---
+++ 18_ArithmaticOperators.java
@@ -0,0 +1,21 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class ArithmaticOperators {
+   public ArithmaticOperators() {
+   }
+
+   public static void main(String[] var0) {
+      int var1 = 10;
+      byte var2 = 20;
+      System.out.println(var1 + var2);
+      System.out.println(var1 % var2);
+      System.out.println(var1 - var2);
+      System.out.println(var1 / var2);
+      System.out.println(var1 * var2);
+      ++var1;
+      System.out.println(var1);
+      System.out.println(var1++);
+      System.out.println(var1--);
+      --var1;
+      System.out.println(var1);
+   }
+}
