9d5efb4c4e304e8492d332df1fbd0b4127ef9742
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-07 23:18:21 +0530

    Create 22_ifElse.java

---
+++ 22_ifElse.java
@@ -0,0 +1,23 @@
+public class elseif {
+    public static void main(String[] args) {
+        int age = 10;
+        
+        if (age>=20) {
+            System.out.println("adult ");
+            
+        }else if(age>=13 && age < 18) {
+            System.out.println("teenager");
+
+        }
+        else{
+            System.out.println("child");
+        }
+    }
+
+    
+}
+
+
+// 4
+
+// agar ek upar ki condition true ho gyi toh hum chhate hai ki niche ki na ho check
