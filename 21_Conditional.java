8249ebaec7fbc8b11b0bb9f3833b99f11596e0ff
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-07 23:17:39 +0530

    Create 21_Conditional.java

---
+++ 21_Conditional.java
@@ -0,0 +1,15 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class ConditionalStatement {
+   public ConditionalStatement() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 22;
+      if (var1 >= 18) {
+         System.out.println("adult");
+      } else {
+         System.out.println("not adult");
+      }
+
+   }
+}
