12a8f5942fe2531937a8b7cf6f9de6fc4cdb40d2
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-07 23:22:43 +0530

    Create 24_evenodd.java

---
+++ 24_evenodd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class evenodd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int number = sc.nextInt();
+        if(number==0){
+            System.out.println("number is neither odd nor even");
+        }if (number%2==0) {
+            System.out.println("number is even");
+            
+        }else{
+            System.out.println("number is odd");
+        }
+        
+    }
+    
+}
+
+
+
+// 3 odd even
