b10f136b4bfb25174588b2f02ef89125912a53b0
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-08 21:01:31 +0530

    Create 26_SwitchCase.java

---
+++ 26_SwitchCase.java
@@ -0,0 +1,16 @@
+public class switchstatement {
+    public static void main(String[] args) {
+        int number = 2;
+        switch(number){
+            case 1 : System.out.println("samosa");
+            case 2 : System.out.println("burger");
+            break;
+            case 3 : System.out.println("mango shake");
+            default: System.out.println("we wake up");
+        }
+    }
+
+
+
+    
+}
