feb889fa9c76ea1dd12dfa9a7b0d36eabcdc29a7
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-08 20:46:25 +0530

    Create 24_ternaryOperator.java

---
+++ 24_ternaryOperator.java
@@ -0,0 +1,19 @@
+public class ternaryOperator {
+
+
+    // variable = condition? statement1:statement2
+    // string type = (5%2==0)? ?+"even": "odd";
+    public static void main(String[] args) {
+        int number= 4;
+        (number%2==0) ? + "even":"odd";
+        System.out.println(type);
+    }
+
+    
+}
+
+
+
+// 3 operands operate
+
+
