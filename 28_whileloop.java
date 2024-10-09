fd281aefc10ecc3fe5b23ac30ced440fb1a5b756
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-09 22:50:34 +0530

    Create 28_whileloop.java

---
+++ 28_whileloop.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.css.Counter;
+
+public class BasicLoop {
+    public static void main(String[] args) {
+        // while(condition){
+        // do something
+        // }
+        int counter =0;
+        while(counter <100){
+            System.out.println("HELLO WORLD");
+            counter++;
+
+        }
+        System.out.println("printed HW 100x");
+    }
+    
+}
+
+
+// whileloop
