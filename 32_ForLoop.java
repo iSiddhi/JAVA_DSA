7165a0b26ded278928f9714e33c7f33e286f0e0f
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-09 23:13:16 +0530

    Create 32_ForLoop.java

---
+++ 32_ForLoop.java
@@ -0,0 +1,12 @@
+public class forLoop {
+    // for(init;condition;updation){
+    // }
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            System.out.println("heello world");
+
+        }
+
+    }
+    
+}
