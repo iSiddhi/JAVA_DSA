086eb4526bc38ec5d1e886473a448ed97011e72e
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-10 22:17:11 +0530

    Create 35_dowhile.java

---
+++ 35_dowhile.java
@@ -0,0 +1,12 @@
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int i =0;
+        do{
+            System.out.println("heelo world");
+            i++;
+
+        }while(i<=10);
+    }
+
+    
+}
