10bb1616375ace755c0c8604f1b3bed7db09cac0
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-11 11:07:13 +0530

    Create 36_NestedLoop.java

---
+++ 36_NestedLoop.java
@@ -0,0 +1,21 @@
+public class nestedLooo {
+    public static void main(String[] args) {
+        for(int i =1;i<=4;i++){
+            for(int star= 1;star<=i;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+    }
+
+    
+}
+
+
+
+// thinking 
+// 1. no. of lines,outerloop=4
+// number of time = inner loop
+// what to print = starts
+// *
