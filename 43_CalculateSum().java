ec913418d86436fc78d445a395538dd3e2f10a32
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-15 01:44:12 +0530

    Create 43_CalculateSum().java

---
+++ 43_CalculateSum().java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class FunctionParameters {
+    public static void Calculatesum(){
+        Scanner sc = new Scanner(System.in);
+        int  a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum= a+b;
+        System.out.println("sum is "+ sum);
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Calculatesum();
+
+
+
+    }
+    
+}
+
+
+
+// returntType name(type para1,type para2){
+// body
+// retrun statement;}
