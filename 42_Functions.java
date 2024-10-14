6d3606cdcaa0e0b2904173a19e04a8eef45390f1
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-15 01:33:44 +0530

    Create 42_Functions.java

---
+++ 42_Functions.java
@@ -0,0 +1,27 @@
+public class functions {
+    public static void printHelloWorld(){
+        System.out.println("hello world");
+
+    }
+        public static void main(String[] args) {
+             printHelloWorld();  
+                //  function call
+
+        
+    }
+
+
+    
+}
+
+
+
+
+// function= block of code which perform particular instruction
+// syntax
+// returnType name(){
+// body
+// return statement
+// }
+// the fuction which returns nothing its return type is void .
+// the functions which is writeen inside  class is called methods.
