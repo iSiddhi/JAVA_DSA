676694cf2f29f074982c7ce9da8b5a34365ed0a1
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-10-16 03:37:28 +0530

    Create 52_Scope.java

---
+++ 52_Scope.java
@@ -0,0 +1,12 @@
+public class methodScope {
+
+
+    
+}
+
+
+// method scope =  kisi bhi function ke andr koi bhi variable define hua hai toh , jaha bhi jis bhi line mai wo variable define hua hai  uske baad us dunction ko hum kahi bhi use kr skte hai
+
+
+
+// block scope = variable if defined in a block can be only used in that block
