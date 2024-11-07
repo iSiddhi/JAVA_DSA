7c8b7b1a3dc049ea1ba71bb8b493bfe0c4627546
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-11-07 23:53:28 +0530

    Create 56_arrayy.java

---
+++ 56_arrayy.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class inputarray {
+    public static void main(String[] args) {
+        int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+        // int phy;
+        // phy = sc.nextInt();
+        marks[0]= sc.nextInt();   //phy
+        marks[1]= sc.nextInt();   //che
+        marks[2]= sc.nextInt();  //maths
+
+        System.out.println("phy "+ marks[0]);
+
+    }
+    
+}
