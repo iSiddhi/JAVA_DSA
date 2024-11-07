08dae647d00ef4fb6a04fd627e4088b99a2b0f53
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-11-07 23:55:18 +0530

    Create 57_arrayBasic.java

---
+++ 57_arrayBasic.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class arrayy {
+    public static void main(String[] args) {
+        // #dataType arrayName= new dataType[size]
+        int marks[] = new int[50];
+        int numbers[] = new int[89];
+        String fruits[]= {"apple","bananas","mangoes"}
+
+
+    }
+    
+}
