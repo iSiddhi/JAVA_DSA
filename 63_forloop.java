2c06a05530ec063200dc1fa23d631b19ea56e5cf
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-22 00:27:09 +0530

    Create 63_forloop.java

---
+++ 63_forloop.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class forloop {
+    
+    public static void main(String[] args) {
+        // for(int i =1;i<10;i++){
+        //     System.out.println(i);
+
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // Integer n = sc.nextInt();
+
+        // for(int i = 1;i<n;i++){
+        //     System.out.println(i);
+        // }
+
+        // for(int i=1 ; i<5;i++)
+        // System.out.println("****");
+
+        
+
+
+    }
+}
