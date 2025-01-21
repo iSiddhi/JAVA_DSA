3f44e385a7392791ea3ffd573272f47277b524e6
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-21 23:30:10 +0530

    Create 59_conditions.java

---
+++ 59_conditions.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class conditionals {
+    public static void main(String[] args) {
+    //    int  a=10;
+    //    int  b=9;
+       Scanner sc = new Scanner(System.in);
+       Integer a = sc.nextInt();
+       Integer b= sc.nextInt();
+       if(a>b){
+        System.out.println("a is greater");
+
+       }else{
+        System.out.println("b>a");
+       }
+       System.out.println(a);
+       
+
+
+
+    }    
+}
