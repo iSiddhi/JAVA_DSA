ea7a5972e338ce3afee47aea1c8e5ea9ba6f9331
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-22 00:06:40 +0530

    Create 62_while.java

---
+++ 62_while.java
@@ -0,0 +1,13 @@
+public class looop {
+    public static void main(String[] args) {
+        int i = 0;
+        // int b;
+        while(i<100){
+            System.out.println("hello world");
+            i ++;
+
+
+        }
+    }
+    
+}
