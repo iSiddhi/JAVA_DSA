a93bb12a3c74749d7ad2094511132af6a46242b5
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-21 23:42:02 +0530

    Create 60_elseif.java

---
+++ 60_elseif.java
@@ -0,0 +1,18 @@
+public class elseif {
+    public static void main(String[] args) {
+        int a = 10;
+        int b=9;
+        if (a>b) {
+            System.out.println(a);
+        
+            
+        }
+        else if(b>a){
+            System.out.println("b");
+        }
+        else{
+            System.out.println("a=b");
+        }
+    }
+    
+}
