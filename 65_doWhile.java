18eecf035c557c5d38ef01ac1f1da7debfb71233
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-23 01:16:21 +0530

    Create 65_doWhile.java

---
+++ 65_doWhile.java
@@ -0,0 +1,13 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println("HELLO WORLD");
+            i++;
+
+        }while(i<=10);
+
+    }
+
+    
+}
