7a08feb3a57b5e4516e1b9ab1977d971c2bd347d
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-23 01:32:25 +0530

    Create 67_multipleof10.java

---
+++ 67_multipleof10.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class multiples {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        do{
+            System.out.println("enter the numbers");
+        int n= sc.nextInt();
+
+        if(n%10==0){
+            continue;
+        }
+        System.out.println(n);
+        }while(true);
+        
+    }
+    
+}
