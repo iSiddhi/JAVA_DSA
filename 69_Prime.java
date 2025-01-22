c594bbd1e625dc648c53e58cf61a7b24386f264a
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-23 01:39:45 +0530

    Create 69_Prime.java

---
+++ 69_Prime.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class prime {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("enter the number ");
+            int n = sc.nextInt();
+
+            if (n%2==0) {
+                continue;
+                
+            }
+            System.out.println("yes");
+
+        
+        }
+        while(true); 
+            
+        
+
+        
+    }
+    
+}
