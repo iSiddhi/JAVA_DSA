5878ba341729c345a840a1251cceb533d1e37370
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-23 22:03:36 +0530

    Create 71_characterpattern.java

---
+++ 71_characterpattern.java
@@ -0,0 +1,19 @@
+public class charac {
+    public static void main(String[] args) {
+        int n=4;
+        char ch ='A';
+        for(int line = 1;line<=n;line++){
+            for(int cha = 1;cha <=line;cha++) {
+                System.out.print(ch);
+                ch++;
+                
+
+                // System.out.print("");
+                // System.out.print("AB");
+
+            }
+            System.out.println();
+        }
+    }
+    
+}
