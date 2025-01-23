56a50b4c5c3341b3bce25442d3d0636b48af3aa5
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-23 22:40:14 +0530

    Create 72_funct.java

---
+++ 72_funct.java
@@ -0,0 +1,14 @@
+public class funct {
+
+    public static void printHello(){
+        System.out.println("hello world ");
+    }
+
+    public static void main(String[] args) {
+        // System.out.println("hello world");
+        printHello();
+
+
+    }
+    
+}
