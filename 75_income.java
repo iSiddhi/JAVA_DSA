20b3532ffd8eb267292667ee0dae62da77e3d7c7
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-24 22:57:40 +0530

    Create 75_income.java

---
+++ 75_income.java
@@ -0,0 +1,17 @@
+public class income {
+
+    public static int Calculatesalary(int Siddhi,int Shruti){
+        int salary= Siddhi+ Shruti;
+        return salary;
+    }
+    public static void main(String[] args) {
+        // int updates = Calculatesalary(0, 0);
+        int  Siddhi = 56;
+        int Shruti= 72;
+        int updates = Calculatesalary(Siddhi, Shruti);
+
+        System.out.println(updates);
+    }
+
+    
+}
