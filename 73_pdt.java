cf43c1abb0b82fcd8849baeb7c5392ab80fc4cf3
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-24 22:47:38 +0530

    Create 73_pdt.java

---
+++ 73_pdt.java
@@ -0,0 +1,15 @@
+public class productttt {
+
+    public static int pdt(int a ,int b){
+        int product= a*b;
+        return product;
+
+    }
+    public static void main(String[] args) {
+        int a = 3;
+        int b= 5;
+        int prod=pdt(3,5);
+        System.out.println(prod);
+        }
+    
+}
