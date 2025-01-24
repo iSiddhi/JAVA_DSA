2688534e2cf9b12b7650eb74e5dcd80695c6cd4d
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2025-01-25 00:08:48 +0530

    Create 78_methods.java

---
+++ 78_methods.java
@@ -0,0 +1,10 @@
+public class methods {
+    static void myMethod(){
+        System.out.println("i just got executed ");
+
+    }
+    public static void main(String[] args) {
+        myMethod();
+    }
+    
+}
